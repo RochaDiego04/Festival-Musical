90df3aa0007171742f55443e51122f3731974959
Diego Rocha <drochaesc@gmail.com>
2023-06-04 20:47:14 -0600

    Contenido de festival
    automatizando conversión de imágenes a otros formatos usando gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,17 @@ require("gulp") extrae la clave gulp del .json => "gulp": "^4.0.2", su funcional
 src para identidicar un archivo
 dest para guardarlo
 */
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require("gulp-plumber");
+
+// Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 
 function css( done ) {
@@ -18,10 +26,45 @@ function css( done ) {
     done(); //Callback que avisa a gulp cuando llegamos al final
 }
 
+function imagenes( done ) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin (opciones) ) )
+        .pipe( dest('build/img'))
+    done();
+}
+
+function versionWebp( done ) {
+    const opciones = {
+        quality: 50
+    };
+    
+    src('src/img/**/*.{png,jpg}') //Busca todas las imagenes con estos 2 formatos
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img'))
+    done();
+}
+
+function versionAvif( done ) {
+    const opciones = {
+        quality: 50
+    };
+    
+    src('src/img/**/*.{png,jpg}') //Busca todas las imagenes con estos 2 formatos
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img'))
+    done();
+}
+
 function dev( done ) {
     watch("src/scss/**/*.scss", css)
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
